c663616adbf287adef389d9c26e7cbbe7c7d1874
jarooda <jaluwibowoaji@gmail.com>
2021-03-11 12:08:52 +0700

    feat: mark as done a task

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card mb-3">
+  <div :class="`card mb-3 ${data.status ? 'bg-dark text-white' : ''}`">
     <div class="card-header">
       {{ data.title }}
     </div>
@@ -7,11 +7,12 @@
       <blockquote class="blockquote mb-0">
         <p>{{ data.description }}</p>
         <footer class="blockquote-footer">
-          {{ data.status ? "Done" : "Not Done" }} || Priorities: {{ data.priorities }} || Due Date: {{ indoFormatDate }}
+          <span class="badge badge-success">{{ data.status ? "Done" : "Not Done" }}</span> || <span class="badge badge-primary">Priorities: {{ data.priorities }}</span> || <span class="badge badge-info">Due Date: {{ indoFormatDate }}</span>
         </footer>
-        <div class="mt-2 d-flex flex-row-reverse">
+        <div class="mt-2 d-flex flex-column-reverse flex-md-row-reverse">
           <button class="btn btn-danger m-2" @click="destroy">Delete</button>
           <button class="btn btn-warning m-2" @click="edit">Edit</button>
+          <button class="btn btn-success m-2" @click="editStatus">{{ data.status ? "Mark as Not Done" : "Mark as Done" }}</button>
         </div>
       </blockquote>
     </div>
@@ -35,7 +36,13 @@ export default {
     },
     edit () {
       this.$store.commit('SET_IS_EDIT', true)
-      this.$store.commit('SET_TASK_BULK', this.data)
+      this.$store.commit('SET_TASK_BULK', { ...this.data })
+    },
+    editStatus () {
+      this.$store.dispatch('EDIT_TASK', {
+        id: this.data.id,
+        status: !this.data.status
+      })
     }
   }
 }
